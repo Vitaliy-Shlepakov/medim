9973df9471a7c890616888d0ed7b32e8d0ab99ba
Виталий Шлепаков <shlepakov58@gmail.com>
2020-02-22 11:04:33 +0200

    add pagination to project

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { CurrentUserContext} from "../../contexts/currectUser";
 
 const Header = () => {
+  const [currentUserState] = useContext(CurrentUserContext);
+
   return (
     <div className="Header">
       <nav className="navbar navbar-light">
@@ -15,16 +18,43 @@ const Header = () => {
                 Home
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink to="/login" className="nav-link" exact>
-                Sing In
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/register" className="nav-link" exact>
-                Sing up
-              </NavLink>
-            </li>
+            {
+              currentUserState.isLoggedIn === false
+              &&
+              <>
+                <li className="nav-item">
+                  <NavLink to="/login" className="nav-link" exact>
+                    Sing In
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/register" className="nav-link" exact>
+                    Sing up
+                  </NavLink>
+                </li>
+              </>
+            }
+            {
+              currentUserState.isLoggedIn &&
+                <>
+                <li className="nav-item">
+                  <NavLink to="/articles/new" className="nav-link" exact>
+                    <i className="ion-compose"></i>
+                    &nbsp; New post
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to={`/profiles/${currentUserState.currentUser.username}`} className="nav-link" exact>
+                    <img
+                      src={currentUserState.currentUser.image}
+                      className="user-pic"
+                      alt=""
+                    />
+                    &nbsp;{currentUserState.currentUser.username}
+                  </NavLink>
+                </li>
+              </>
+            }
           </ul>
         </div>
       </nav>
--- src/pages/globalFeet/index.js
+++ src/pages/globalFeet/index.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import Feed from "../../components/Feed";
+import useFetch from '../../hooks/useFetch';
+import Pagination from "../../components/Pagination";
+import { getPaginator, limit } from "../../utils";
+import { stringify } from "query-string";
 
-const GlobalFeet = () => {
-  return (
-    <div className="GlobalFeet
-    ">
-      GlobalFeet
-    </div>
-  );
+const GlobalFeet = props => {
+    //формируем строку запроса
+    const {currentPage, offset} = getPaginator(props.location.search);
+    const stringifiedParams = stringify({
+        limit,
+        offset
+    });
+    const apiUrl = `/articles?${stringifiedParams}`;
+    const url = props.match.url;
+    const [{response, isLoading, error}, doFetch] = useFetch(apiUrl);
+
+    useEffect(() => {
+        doFetch()
+    }, [doFetch, currentPage]);
+
+    return (
+        <div className="home-page">
+            <div className="banner">
+                <div className="container">
+                    <h1>Medium clone</h1>
+                    <p>A place to share knowledge</p>
+                </div>
+            </div>
+            <div className="container page">
+                <div className="row">
+                    <div className="col-md-9">
+                        { isLoading && <div>Loading ...</div> }
+                        { error && <div>Some error happened ...</div> }
+                        { !isLoading && response &&
+                            <>
+                                <Feed articles={response.articles}/>
+                                <Pagination
+                                    total={response.articlesCount}
+                                    limit={limit}
+                                    currentPage={currentPage}
+                                    url={url}
+                                />
+                            </>
+                        }
+                    </div>
+                    <div className="col-md-3">
+                        Popular tags
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
 };
 
 export default GlobalFeet;
